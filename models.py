fixed error in sigma sampling calculation

models.py
@@ -14,8 +14,8 @@ class Encoder(nn.Module):
     def __init__(self, z_dim = 64) -> None:
         super().__init__()
         self.res = ResNet(3,64,useMaxPool=True)
-        self.res1 = ResNet(64,256,useMaxPool=True)
-        self.res2 = ResNet(256,512,useMaxPool=True)
+        self.res1 = ResNet(64,128,useMaxPool=True)
+        self.res2 = ResNet(128,512,useMaxPool=True)
         #self.ln = nn.Linear(None,512)
         self.mu = nn.Linear(512,z_dim)
         self.sigma = nn.Linear(512,z_dim)
@@ -33,11 +33,11 @@ class Encoder(nn.Module):
         x = nn.Linear(flattened_size,512).to(x.device)(x)
 
         mu = self.mu(x)
-        sigma = self.sigma(x)
+        l_sigma = self.sigma(x)
 
-        z = mu + sigma * torch.rand_like(sigma)
+        z = mu + torch.exp(l_sigma/2)*torch.rand_like(l_sigma)
 
-        return mu,sigma,z
+        return mu,l_sigma,z
 
 
 class Decoder(nn.Module):
@@ -73,11 +73,11 @@ class VAE(nn.Module):
         self.z_dim = z_dim
     
     def forward(self,x):
-        mu,sigma,z = self.encoder.forward(x)
+        mu,l_sigma,z = self.encoder.forward(x)
 
         out = self.decoder.forward(z)
 
-        return mu,sigma,out
+        return mu,l_sigma,out
     
     def inferenceR(self,should_save = True):
         z_var = torch.rand(1,self.z_dim).to(self.device)

train.py
@@ -28,11 +28,11 @@ def reshape_img(img,size = (64,64)):
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-def kl_loss(mu,sigma):
-    kl_loss = -0.5 * torch.sum(1 + torch.log(sigma) - mu**2 - sigma**2)
+def kl_loss(mu,l_sigma):
+    kl_loss = -0.5 * torch.sum(1 + l_sigma - mu**2 - torch.exp(l_sigma))
     return kl_loss
 
-def training_loop(n_epochs, optimizer, model, loss_fn, device,l_weight = 1000,  
+def training_loop(n_epochs, optimizer, model, loss_fn, device,l_weight = 10,  
                   epoch_start = 0, batch_size = 64, 
                   data_length = 4000,max_grad_norm=1.0):
     data_idx = list(range(0,data_length))
@@ -130,6 +130,6 @@ if __name__ == "__main__":
         loss_fn = loss_fn,
         device = device,
         batch_size = 16,
-        epoch_start = 241,
+        epoch_start = 1,
         data_length = 10_000
     )